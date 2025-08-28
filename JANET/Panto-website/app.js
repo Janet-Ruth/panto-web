654edb26820c107e98fa5752b0fc04612fa5a334
Janet-Ruth <janetruth24@gmail.com>
2025-08-29 00:59:52 +0100

    Add files via upload

---
+++ JANET/Panto-website/app.js
@@ -0,0 +1,37 @@
+// const cards = document.querySelectorAll(".card");
+// console.log(cards)
+// const leftBtn = document.querySelector(".left");
+// const rightBtn = document.querySelector(".right");
+
+
+
+// let counter = 0; 
+
+
+// function showCard(index) {
+//   cards.forEach((card, i) => {
+//     card.style.display = (i === index) ? "block" : "none";
+//   });
+// }
+
+
+// showCard(counter);
+
+
+// rightBtn.addEventListener("click", () => {
+//   counter++;
+//   if (counter >= cards.length) {
+//     counter = 0; 
+//   }
+//   showCard(counter);
+// });
+
+
+// leftBtn.addEventListener("click", () => {
+//   counter--;
+//   if (counter < 0) {
+//     counter = cards.length - 1; 
+//   }
+//   showCard(counter);
+// });
+
